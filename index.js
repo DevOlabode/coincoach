5cd7454077cbf9916b7cb8c7f87b1284a01b6078
Samuel Olabode <Solabode499@gmail.com>
2025-12-05 20:50:24 -0700

    Add initial server setup and home page view with boilerplate layout

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+const ejsMate = require('ejs-mate');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended : true}));
+app.engine('ejs', ejsMate);
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{ 
     console.log(`Server is running on port ${PORT}`);
