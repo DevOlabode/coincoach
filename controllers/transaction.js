dc97ef3396cf0b59ba8dee605d72bff9b3703a15
Samuel Olabode <Solabode499@gmail.com>
2025-12-08 18:41:59 -0700

    Implement transaction creation functionality and update form method to POST

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -2,4 +2,8 @@ const transactionSchema = require('../models/transactions');
 
 module.exports.newTransactionForm = (req, res) => {
     res.render('transactions/new');
-}
+};
+
+module.exports.createTransaction = async (req, res, next) => {
+    res.send(req.body);
+};
--- routes/transaction.js
+++ routes/transaction.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/transaction');
 
+const {isLoggedIn} = require('../middleware');
+const catchAsync = require('../utils/catchAsync');
+
 router.get('/new', controller.newTransactionForm);
+
+router.post('/', catchAsync(controller.createTransaction));
 
 module.exports = router;
