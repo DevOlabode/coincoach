27b703522e15289c4aecd097f1da325d68f669b1
Samuel Olabode <Solabode499@gmail.com>
2025-12-08 19:40:35 -0700

    Refactor createTransaction method to save transaction data and handle success response

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,9 +1,20 @@
-const transactionSchema = require('../models/transactions');
+const Transaction = require('../models/transactions');
 
 module.exports.newTransactionForm = (req, res) => {
     res.render('transactions/new');
 };
 
 module.exports.createTransaction = async (req, res, next) => {
-    res.send(req.body);
+    const { amount, type, date, description, category } = req.body;
+    const transaction = new Transaction({
+        userId: req.user._id,
+        amount,
+        type,
+        date,
+        description,
+        category
+    });
+    await transaction.save();
+    req.flash('success', 'Transaction recorded successfully!');
+    res.redirect('/'); 
 };
