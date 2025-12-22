6b93f973dc1a84b9b227e009f82cd1e12d637a5d
Samuel Olabode <Solabode499@gmail.com>
2025-12-22 16:07:26 -0700

    Made the insight AI work in the codebase

--- controllers/insights.js
+++ controllers/insights.js
@@ -1,9 +1,11 @@
-const Transactions = require('../models/transactions');
+const Transaction = require('../models/transactions');
 
 const transactionInsight = require('../services/insightAI')
 
 module.exports.allInsights =  async (req, res)=>{
-    const transactions = await Transactions.find({user : req.user._id});
+    const transactions = await Transaction.find({ userId : req.user._id });
+    const count = await Transaction.countDocuments({userId : req.user._id});
     const insight = await transactionInsight(transactions);
-    res.render('insights/all', {insight, transactions})
+    res.send(insight);
+    
 };
