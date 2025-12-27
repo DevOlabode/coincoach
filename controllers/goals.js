6624c421f5764a29fe758552029a07e4a143b257
Samuel Olabode <Solabode499@gmail.com>
2025-12-26 22:34:56 -0700

    Implementing the financial goal to the codebase

--- controllers/goals.js
+++ controllers/goals.js
@@ -2,13 +2,13 @@ const Goals = require('../models/goals');
 const Transactions = require('../models/transactions')
 const goalsAI = require('../services/goalsAI')
 
-module.exports.index = (req, res)=>{
-    res.render('goals/index')
+module.exports.index = async (req, res)=>{
+    const goals = await Goals.find({userId : req.user._id})
+    res.render('goals/index', {goals});
 };
 
 module.exports.goals = async(req, res)=>{
     const {explanation} = req.body;
     const transactions = await Transactions.find({userId : req.user._id})
     const output = await goalsAI(explanation, transactions);
-    res.send(output)
 };
