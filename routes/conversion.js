0d46ea340d13ac5d9c27fb4b303b830d727c3776
Samuel Olabode <Solabode499@gmail.com>
2025-12-29 15:19:05 -0700

    Implementing the currency conversion feature to this app

--- routes/conversion.js
+++ routes/conversion.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const controller = require('../controllers/conversion');
+
+router.get('/', controller.index)
+
 module.exports = router;
